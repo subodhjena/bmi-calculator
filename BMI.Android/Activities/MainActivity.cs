2298da670a3e9f16c24bcebd700579dac97b1b00
Subodh Jena <jenasubodh@outlook.com>
2016-03-27 00:16:11 +0530

    Adding Settings Page on Android
    Adding Settings Page on Android

---
+++ BMI.Android/Activities/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using Android.App;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Android.Support.V7.App;
+using V7Toolbar = Android.Support.V7.Widget.Toolbar;
+
+namespace BMI.Android.Activities
+{
+    [Activity(Label = "BMI", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate (savedInstanceState);
+            SetContentView (Resource.Layout.Activity_Main);
+
+            var toolbar = FindViewById<V7Toolbar> (Resource.Id.toolbar);
+
+            SetSupportActionBar (toolbar);
+            SupportActionBar.Title = "BMI Calculator";
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.main_menu, menu);
+            return base.OnPrepareOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+
+            switch (item.ItemId)
+            {
+                case Resource.Id.action_settings:
+                    StartSettingsActity();
+                    return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void StartSettingsActity()
+        {
+            StartActivity(typeof(SettingsActivity));
+        }
+    }
+}
+
+
--- BMI.Android/Activities/SettingsActivity.cs
+++ BMI.Android/Activities/SettingsActivity.cs
@@ -7,21 +7,23 @@ using Android.OS;
 using Android.Support.V7.App;
 using V7Toolbar = Android.Support.V7.Widget.Toolbar;
 
-namespace BMI.Android
+namespace BMI.Android.Activities
 {
-    [Activity(Label = "BMI", MainLauncher = true, Icon = "@mipmap/icon")]
-    public class MainActivity : AppCompatActivity
+    [Activity(Label = "Settings")]
+    public class SettingsActivity : AppCompatActivity
     {
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate (savedInstanceState);
-            SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Activity_Settings);
 
             var toolbar = FindViewById<V7Toolbar> (Resource.Id.toolbar);
 
             SetSupportActionBar (toolbar);
-            SupportActionBar.Title = "BMI Calculator";
+            SupportActionBar.Title = "Settings";
+            SupportActionBar.SetDisplayHomeAsUpEnabled (true);
+            SupportActionBar.SetHomeButtonEnabled (true);
+
         }
     }
 }

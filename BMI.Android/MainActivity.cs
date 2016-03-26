ff7f601fe4b531fccb5f71ea61ef50c34238cbb8
Subodh Jena <jenasubodh@outlook.com>
2016-03-26 23:50:19 +0530

    Initial Commit
    Initial Commit, Android UI

---
+++ BMI.Android/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Android.App;
+using Android.Widget;
+using Android.OS;
+
+using Android.Support.V7.App;
+using V7Toolbar = Android.Support.V7.Widget.Toolbar;
+
+namespace BMI.Android
+{
+    [Activity(Label = "BMI", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate (savedInstanceState);
+            SetContentView (Resource.Layout.Main);
+
+            var toolbar = FindViewById<V7Toolbar> (Resource.Id.toolbar);
+
+            SetSupportActionBar (toolbar);
+            SupportActionBar.Title = "BMI Calculator";
+        }
+    }
+}
+
+
